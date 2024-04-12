1a105fefd0821500ca1403acad772949c70f8d05
Guillaume <millecactus@gmail.com>
2024-04-12 17:24:37 +0200

    adding actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+const BASE_URL = "http://localhost:4321"
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://millecactus.github.io',
-	base :'dreamscape'
+	site: BASE_URL,
+	base:'',
 	integrations: [mdx(), sitemap()],
 });
