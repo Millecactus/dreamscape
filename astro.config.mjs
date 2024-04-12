6135f3c458ed2f39082b0129cc2c50b8d88f51db
Guillaume <millecactus@gmail.com>
2024-04-12 15:04:42 +0200

    configuration astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://millecactus.github.io',
+	base :'dreamscape'
 	integrations: [mdx(), sitemap()],
 });
